6f5dc0114a99816de6d4213f08fa8202278f486e
AyuburRahman <aiburrahman17@gmail.com>
2022-06-14 11:53:07 +0600

    completed

--- src/screens/Details/Details.js
+++ src/screens/Details/Details.js
@@ -1,18 +1,43 @@
-import { View, SafeAreaView ,StyleSheet, ScrollView } from 'react-native';
+import { View, SafeAreaView ,StyleSheet, ScrollView, Pressable, Linking, } from 'react-native';
 import React from 'react';
 import Text from '../../components/text/Text';
 import PlannetHeader from '../../components/PlanetHeader/PlannetHeader';
 import { colors } from '../../theme/colors';
 import { spacing } from '../../theme/spacing';
-import {  MercurySvg, EarthSvg, JupiterSvg, MarsSvg, NeptuneSvg, SaturnSvg, UranusSvg, VenusSvg } from '../../svg';
+import {  
+     MercurySvg,
+     EarthSvg, 
+     JupiterSvg,
+     MarsSvg, 
+     NeptuneSvg,
+     SaturnSvg, 
+     UranusSvg, 
+     VenusSvg,
+     
+   } from '../../svg';
 
+    const PlanetSection = ({title, value}) => {
+       return(
+          <View style={styles.planetSection}>
+             <Text preset='small' style={{ textTransform:"uppercase" }}>{title}</Text>
+             <Text preset='h2'>{value}</Text>
+          </View>
+       )
+    }
 
-export default function Details({navigation, route}) {
+export default function Details({route}) {
   const planet = route.params.planet;
   // console.log("planet -->", route);
-  const {name} = planet;
+  const {
+   name, 
+   description, 
+   rotationTime, 
+   revolutionTime, 
+   radius, 
+   avgTemp, 
+   wikiLink } = planet;
 
-  const renderImage = (name) => {
+  const renderImage = () => {
       switch(name){
          case "mercury":
             return <MercurySvg />;
@@ -33,13 +58,38 @@ export default function Details({navigation, route}) {
       }
   };
 
+  const onPressLink = () => {
+       Linking.openURL(wikiLink);
+  }
+
   return (
     <SafeAreaView style={styles.container}>
         <PlannetHeader  backBtn={true} />
         <ScrollView>
            <View style={styles.imageView}>
-               {renderImage(name)}
+               {renderImage()}
            </View>
+
+           <View style={styles.detailsView} >
+              <Text preset='h1' style={styles.name} >
+                 {name}
+              </Text>
+              <Text style={styles.description}>
+                    {description}
+              </Text>
+              <Pressable onPress={onPressLink} style={styles.source} >
+                 <Text>Source:</Text>
+                 <Text preset='h4' style={styles.wikipedia} > Wikipedia</Text>
+              </Pressable>
+           </View>
+
+            <View style={{height:40}} />
+
+           <PlanetSection title="ROTATION TIME" value={rotationTime} />
+           <PlanetSection title="REVOLUTION TIME" value={revolutionTime} />
+           <PlanetSection title="RADIUS" value={radius} />
+           <PlanetSection title="AVERAGE TEMP." value={avgTemp} />
+
         </ScrollView>
     </SafeAreaView>
   )
@@ -52,8 +102,43 @@ const styles = StyleSheet.create({
         backgroundColor:colors.black,
     },
    imageView:{
-      padding:spacing[5],
+      marginTop:spacing[8],
       alignItems:"center",
       justifyContent:"center",
+    },
+    detailsView:{
+      marginTop:spacing[10],
+      marginHorizontal:spacing[6],
+      alignSelf:"center",
+    },
+    name:{
+       textTransform:"uppercase",
+       textAlign:"center",
+    },
+    description:{
+      textAlign:"center",
+      marginTop:spacing[5],
+      lineHeight:21,
+    },
+    source:{
+      flexDirection:"row",
+      justifyContent:"center",
+      alignItems:"center",
+      marginTop:spacing[5],
+    },
+    wikipedia:{
+      textDecorationLine:'underline',
+      fontFamily:"bold",
+    },
+    planetSection:{
+      flexDirection:"row",
+      alignItems:"center",
+      justifyContent:"space-between",
+      paddingHorizontal:spacing[5],
+      paddingVertical:spacing[4],
+      borderWidth:1,
+      borderColor:colors.grey,
+      marginHorizontal:spacing[6],
+      marginBottom:spacing[4],
     }
 })
--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,5 +1,12 @@
-import { View, FlatList, SafeAreaView, StyleSheet, Pressable } from 'react-native';
-import React from 'react';
+import { 
+  View, 
+  FlatList, 
+  SafeAreaView, 
+  StyleSheet, 
+  Pressable ,
+  TextInput,
+} from 'react-native';
+import React, {useState} from 'react';
 import Text from '../../components/text/Text';
 import PlannetHeader from '../../components/PlanetHeader/PlannetHeader';
 import { colors } from '../../theme/colors';
@@ -25,19 +32,43 @@ const PlanetItem = ({item}) => {
 }
 
 export default function Home({navigation}) {
+  const [list, setList] = useState(PLANET_LIST);
 
   const renderItem = ({item}) => {
-        return(
-            <PlanetItem item={item} />
-         );
-  }
+        return(<PlanetItem item={item} /> );
+  };
+
+  //pass kora text argument ta ke ei function nilo then 
+  const searchFilter = (text) => {
+    //filter ta kortesi holo main array theke
+    const filteredList = PLANET_LIST.filter(item => {
+      //just nam ta match korte hobe tai nam ta niye aslam filter korar por
+      const itemName = item.name.toLowerCase();
+      //user typed text
+      const userTypedText = text.toLowerCase();
+      // >-1 mane milse
+      return itemName.indexOf(userTypedText) > -1;
+    });
+    setList(filteredList);
+  };
 
   return (
     <SafeAreaView style={styles.container}>
        <PlannetHeader />
+
+       <TextInput 
+          placeholder="Type the planet name"
+          placeholderTextColor={colors.white}
+          autoCorrect={false}
+          style={styles.searchInput}
+          onChangeText={(text) => searchFilter(text)}  // মানে text আরগুমেন্ট টা কে searchFilter ফাংশনে পাস করে দাও
+          
+          />
+
+
        <FlatList 
           contentContainerStyle={styles.list}
-          data={PLANET_LIST}
+          data={list}
           keyExtractor={(item,index) => item.name}
           renderItem={renderItem}
           ItemSeparatorComponent = { () => <View style={styles.separator}/>}
@@ -73,6 +104,14 @@ const styles = StyleSheet.create({
      borderWidth:0.5,
      borderBottomColor:colors.white,
 
+    },
+    searchInput:{
+      padding:spacing[4],
+      color:colors.white,
+      borderBottomColor:colors.white,
+      borderBottomWidth:1,
+      margin:spacing[5],
+      
     }
     
 })
